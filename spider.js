a107eac96a86f7d25f3bd857daf23b549e69a858
favers <favers0601@gmail.com>
2017-03-08 21:25:36 +0800

    node git add test

--- spider.js
+++ spider.js
@@ -3,9 +3,22 @@
 const request = require('superagent');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const exec = require('child_process').exec; 
 
 const URL = 'https://github.com/trending/javascript';
 
 function requestHtml() {
     return request.get(URL).then(res => res.text);
 }
+
+async function gitAddCommitPush(date,fileName){
+    // let gitAdd = `git add ${fileName}`;
+    // let gitCommit = `git commit -m ${date}`;
+    // let gitPush = 'git push -u origin master';
+
+    await exec('git add -A')
+    await exec('git commit -m "node git add test"')
+    await exec('git push -u origin master')
+}   
+
+gitAddCommitPush('2017-03-08 test add','spider.js');
