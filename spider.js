0aab982b53d89484d948d138f06ff87e93362050
favers <favers0601@gmail.com>
2017-03-10 10:22:34 +0800

    add 2017-3-10

--- spider.js
+++ spider.js
@@ -8,12 +8,15 @@ const shell = require('shelljs');
 
 const URL = 'https://github.com/trending/javascript';
 
+let date = new Date();
+let formatDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
 function requestHtml() {
     return request.get(URL).then(res => res.text);
 }
 
 function gitAddCommitPush(date) {
-    if (shell.exec('git add -A && git commit -m "test shell" && git push').code === 0) {
+    if (shell.exec(`git add -A && git commit -m "add ${date}" && git push`).code === 0) {
         console.log('提交成功');
     }
 }
@@ -23,7 +26,7 @@ function getData(html) {
     return Array.from($('.repo-list li').map(function () {
         let link = $(this).find('h3 a').attr('href');
         let desc = $(this).find('.py-1 p').text().trim();
-        return({
+        return ({
             link,
             desc
         });
@@ -31,14 +34,17 @@ function getData(html) {
 }
 
 async function start() {
-    console.log('开始');
+    console.log('开始爬取数据');
     let html = await requestHtml();
     let data = getData(html);
     console.log(data);
+    console.log('开始提交数据');
+    gitAddCommitPush(formatDate);
+    console.log('结束本次任务');
 }
 
 
-(function(){
+(function () {
     schedule.scheduleJob('30 * * * * *', function () {
         start();
     })
