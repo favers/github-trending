822b0e9c5102e123030a6cacaf957fae74e6b25f
favers <favers0601@gmail.com>
2017-03-10 10:34:45 +0800

    add 2017-3-10

--- spider.js
+++ spider.js
@@ -6,21 +6,26 @@ const fs = require('fs');
 const schedule = require('node-schedule');
 const shell = require('shelljs');
 
+// 抓取url地址
 const URL = 'https://github.com/trending/javascript';
 
+// 格式化当前日期
 let date = new Date();
 let formatDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
+// 获取页面内容
 function requestHtml() {
     return request.get(URL).then(res => res.text);
 }
 
+// 提交采集的数据到git
 function gitAddCommitPush(date) {
     if (shell.exec(`git add -A && git commit -m "add ${date}" && git push`).code === 0) {
         console.log('提交成功');
     }
 }
 
+// 获取页面数据
 function getData(html) {
     let $ = cheerio.load(html);
     return Array.from($('.repo-list li').map(function () {
@@ -33,11 +38,28 @@ function getData(html) {
     }));
 }
 
+// 将数据写入文件
+function wirteFile(date, data) {
+    let writeStr = `### ${date} \n`;
+    data.map((item)=>{
+        writeStr += `[${item.link}](https://github.com/${item.link}):${item.desc}`
+    });
+
+    fs.writeFile(`./${date}.md`, writeStr, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('写入文件结束');
+    })
+}
+
+// 开始程序
 async function start() {
     console.log('开始爬取数据');
     let html = await requestHtml();
     let data = getData(html);
-    console.log(data);
+    await wirteFile(formatDate, data);
+    // console.log(data);
     console.log('开始提交数据');
     gitAddCommitPush(formatDate);
     console.log('结束本次任务');
