b43fc8b9db37e9f1c399d53a8a56006d925a79bb
favers <favers0601@gmail.com>
2017-03-10 10:04:13 +0800

    add

--- spider.js
+++ spider.js
@@ -6,28 +6,40 @@ const fs = require('fs');
 const schedule = require('node-schedule');
 const shell = require('shelljs');
 
-const exec = require('child_process').exec; 
-
 const URL = 'https://github.com/trending/javascript';
 
 function requestHtml() {
     return request.get(URL).then(res => res.text);
 }
 
-async function gitAddCommitPush(date,fileName){
-    // let gitAdd = `git add ${fileName}`;
-    // let gitCommit = `git commit -m ${date}`;
-    // let gitPush = 'git push -u origin master';
+function gitAddCommitPush(date) {
+    if (shell.exec('git add -A && git commit -m "test shell" && git push').code === 0) {
+        console.log('提交成功');
+    }
+}
 
-    await exec('git add -A')
-    await exec('git commit -m "exec git add test"')
-    await exec('git push')
-}   
+function getData(html) {
+    let $ = cheerio.load(html);
+    return Array.from($('.repo-list li').map(function () {
+        let link = $(this).find('h3 a').attr('href');
+        let desc = $(this).find('.py-1 p').text().trim();
+        return({
+            link,
+            desc
+        });
+    }));
+}
 
-// gitAddCommitPush('2017-03-08 test add','spider.js');
+async function start() {
+    console.log('开始');
+    let html = await requestHtml();
+    let data = getData(html);
+    console.log(data);
+}
 
-(function scheduleCronstyle(){
-    schedule.scheduleJob('30 * * * * *',function(){
-        shell.exec('git add -A && git commit -m "test shell" && git push');
+
+(function(){
+    schedule.scheduleJob('30 * * * * *', function () {
+        start();
     })
 }())
