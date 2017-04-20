3016740ce89335b17ae3e50d0b28c987de32d312
favers <favers0601@gmail.com>
2017-04-20 20:53:41 +0800

    add 2017-4-20

--- spider.js
+++ spider.js
@@ -66,7 +66,7 @@ async function start() {
 
 
 (function () {
-    schedule.scheduleJob('00 00 12 * * *', function () {
+    // schedule.scheduleJob('00 00 12 * * *', function () {
         start();
-    })
+    // })
 }())
